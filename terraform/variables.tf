0c7389339af9ac056aed66e3e9a0be5f7330246b
ogochukwu-ozotta <ogochukwu.ozotta@gmail.com>
2024-02-26 12:56:34 -0600

    added terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "region" {
+  description = "AWS region"
+  type = string
+  default = "us-east-1"
+}
+
+variable "clusterName" {
+  description = "Name of the EKS cluster"
+  type = string
+  default = "quizapp-eks"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-0c7217cdde317cfec" // Replace with the latest AMI ID for your region
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.large"
+}
+
+variable "instance_keypair" {
+  description = "AWS EC2 keypair that need to be associated with EC2 instance"
+  type        = string
+  default     = "gitopskey"
+}
